ab7eab4d1b2c192da7abcfa16b47d43ba7647d42
Priliv <stadnikd53@gmail.com>
2020-09-19 16:02:14 +0300

    lesson3

---
+++ lesson3/ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            List<int> list = new List<int>();
+          for(int i = 0; i <= 10; i++)
+            {
+                list.Add(i);
+            }
+           
+           /*String str = "0 5 10 15 20";
+            int i = 0;
+            do
+            {
+                Console.Write(i + " ");
+                i += 5;
+            }
+            while (i <= 20);
+            
+               
+                
+            String str2 = "01/01/2008";
+            for (int i= 0; i < str.Length; i=i+4)
+            {
+                Console.WriteLine(str[i]);
+            }
+            for (int i= 0; i < str2.Length; i=i+2)
+            {
+                Console.WriteLine(str[i]);
+            }*/
+        }
+    }   
+}
