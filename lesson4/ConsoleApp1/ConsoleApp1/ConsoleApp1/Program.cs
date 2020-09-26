0c39a782e7451e76e1ced24b0c39964ed1e2191d
Priliv <stadnikd53@gmail.com>
2020-09-26 16:07:38 +0300

    lesson4

---
+++ lesson4/ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<string> l = new List<string>() { "My favorite school subjects" };
+            l.Add("My favorite school subjects");
+            l.Add("My favorite school subjects");
+            l.Add("My favorite school subjects");
+            foreach (string f in l)
+            {
+                  Console.WriteLine("My favorite school subjects");
+                
+            }
+            Console.WriteLine(l.Count);
+            l.Clear();
+        }
+    }
+}   
